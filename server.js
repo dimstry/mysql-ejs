cfc40fcb051423592ddcc3b30a85d1315fadc876
GTM-Winter <dimastriana03@gmail.com>
2022-06-12 02:52:20 +0700

    add delate funtion

--- server.js
+++ server.js
@@ -1,45 +1,72 @@
-const express = require("express")
-const mysql = require("mysql")
-const BodyParser = require("body-parser")
+const express = require("express");
+const mysql = require("mysql");
+const BodyParser = require("body-parser");
 /* deklarasi yg di butuhkan */
 
 const app = express(); 
 /* deklarasi lagi karena express itu function */
-app.use(BodyParser.urlencoded({extended : true }))
+app.use(BodyParser.urlencoded({extended : true }));
 //untuk post form , supaya menangkap hasil input an
 
-app.set("view engine", "ejs")
-app.set("views", "views") /*directory html nya dimana*/
+app.set("view engine", "ejs");
+app.set("views", "views"); /*directory html nya dimana*/
 const db = mysql.createConnection({
   host : "127.0.0.1",
   database : "db_siswa",
   user : "root",
   password : "root",
-})
+});
 /* set up databes*/
 
 db.connect((err) => {
-  if (err) throw err
-  console.log ('database connect...')
+  if (err) throw err;
+  console.log ('database connect...');
   // untuk get data
   app.get("/",(req,res) => {
-  const sql = "SELECT * FROM user"
+  const sql = "SELECT * FROM user";
   db.query(sql, (err, result) => {
     const users =JSON.parse(JSON.stringify(result));
-    res.render("index", {users : users, title : "Dims Code"})
-    })
-  })
+    res.render("index", {users : users, title : "Dims Code"});
+    });
+  });
     // untuk insert data 
     app.post("/tambah",(req,res) => {
-      const insertSql =`INSERT INTO user (nama,kelas) VALUES ('${req.body.nama}','${req.body.kelas}');`
+      const insertSql =`INSERT INTO user (nama,kelas) VALUES ('${req.body.nama}','${req.body.kelas}');`;
       db.query(insertSql, (err,result) => {
-        if(err) throw err
+        if(err) throw err;
         res.redirect("/");
-      })
-    })
-})
+      });
+    });
+    
+    // Delate data 
+    app.get("/delate/:id_user", (req, res) => {
+      const delateSql = `DELETE FROM user WHERE id_user='${req.params.id_user}';`;
+      db.query(delateSql, (err,result) => {
+        if(err) throw err;
+        res.redirect("/");
+      });
+    });
+    
+    // edit data 
+    /*
+    app.get("/edit/:id_user", (req, res) => {
+      const sql = `SELECT nama,kelas FROM user WHERE id_user='${req.params.id_user}';`;
+      console.log(sql)
+      db.query(sql, (err, result) => {
+      const user =JSON.parse(JSON.stringify(result));
+      console.log(user.nama);
+        res.render("edit", {
+          user : user, 
+          title : "Dims Code",
+          nama: user.nama,
+          kelas: user.kelas,
+        });
+      });
+    });
+    */
+});
 /* ini untuk mengkoneksikan */
 
 app.listen(8000, () => {
-  console.log('server siap')
-})
+  console.log('server siap');
+});
